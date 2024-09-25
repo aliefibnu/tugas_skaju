953af390b31efae311db84b391b919ac7117aa21
Alief Ibnu <aliefarumi@gmail.com>
2024-09-25 21:25:30 +0700

    hapus file tes

--- coding_pak_ryan/koneksi_tes1.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$host = "localhost";
-$username = "root";
-$password = "";
-$nama_database = "db_sekolah";
-
-$conn = new mysqli($host, $username, $password, $nama_database);
-var_dump($conn);
