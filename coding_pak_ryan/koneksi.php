e3670281c7bfc57e398693dce557550857bcf17a
Alief Ibnu <aliefarumi@gmail.com>
2024-09-25 21:08:12 +0700

    perjelas komentar

--- coding_pak_ryan/koneksi.php
+++ coding_pak_ryan/koneksi.php
@@ -1,15 +1,15 @@
 <?php
 
 //? Deklarasi Variabel
-$host = "localhost";
+$server = "localhost";
 $username = "root";
 $password = "";
 $nama_database = "db_sekolah";
 
 //? Algoritma koneksi ke databae
-$koneksi_ke_database = mysqli_connect($host, $username, $password, $nama_database) or die("koneksi gagal");
+$koneksi_ke_database = mysqli_connect($server, $username, $password, $nama_database) or die("koneksi gagal");
 
-//? Algoritma menggunakan database yang sudah dipilih : db_sekolah
+//? Algoritma menggunakan database yang sudah dipilih $nama_databse = "db_sekolah";
 $database = mysqli_select_db($koneksi_ke_database, $nama_database);
 
 //? Cek apakah koneksi berhasil atau gagal
