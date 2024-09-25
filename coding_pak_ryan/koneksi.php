6a3a64f855f635fdce09a209bfce4f0208c1a673
Alief Ibnu <aliefarumi@gmail.com>
2024-09-25 21:04:39 +0700

    ubah nama variabel koneksi

--- coding_pak_ryan/koneksi.php
+++ coding_pak_ryan/koneksi.php
@@ -4,10 +4,10 @@ $host = "localhost";
 $username = "root";
 $password = "";
 $nama_database = "db_sekolah";
-$koneksi = mysqli_connect($host, $username, $password, $nama_database) or die("koneksi gagal");
-$database = mysqli_select_db($koneksi, $nama_database);
+$koneksi_ke_database = mysqli_connect($host, $username, $password, $nama_database) or die("koneksi gagal");
+$database = mysqli_select_db($koneksi_ke_database, $nama_database);
 
-if ($koneksi) {
+if ($koneksi_ke_database) {
   echo ('koneksi berhasil');
 } else {
   echo ("koneksi gagal");
