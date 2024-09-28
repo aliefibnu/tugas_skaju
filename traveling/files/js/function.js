3f114d51f57f6386bf6685181cf007c9bba2a4c1
aliefibnu <aliefarumi@gmail.com>
2024-09-28 14:55:23 +0700

    update newyork sebelum animasi transisi

--- traveling/files/js/function.js
+++ traveling/files/js/function.js
@@ -1,7 +1,23 @@
-function updateSizeJudul(){
+function updateSizeJudul() {
   if (window.scrollY < 50) {
+    elTitle.classList.remove("hidden");
+    wrapperTitle.appendChild(elTitle);
     elTitle.style.scale = `${(window.scrollY / 50) * 100 + 50}%`;
+  } else if (window.scrollY > 50 && window.scrollY < 70) {
+    elTitle.classList.remove("hidden");
+    wrapperTitle.appendChild(elTitle);
+    elTitle.style.scale = `${100 + 50}%`;
   } else if (window.scrollY > 70 && window.scrollY < 130) {
+    elTitle.classList.remove("hidden");
+    wrapperTitle.appendChild(elTitle);
     elTitle.style.scale = `${(window.scrollY / 70) * 100 + 50}%`;
+  } else if (window.scrollY > 130) {
+    elTitle.classList.add("hidden");
+    setTimeout(function () {
+      if (window.scrollY > 130) {
+        elTitle.remove();
+      }
+    }, 500);
   }
+  // document.title = window.scrollY;
 }
--- traveling/files/js/index.js
+++ traveling/files/js/index.js
@@ -2,9 +2,63 @@
 
 // ? Deklarasi variabel
 let elTitle = document.querySelectorAll("#home h1")[0];
+let elAnchor = document.getElementById("anchor1");
+let wrapperTitle = document.getElementById("wrapperTitle");
 // ? Set default size untuk teks
 elTitle.style.scale = `${(window.scrollY / 50) * 100 + 50}%`;
 // ? Logika kalau window di scroll
 document.addEventListener("scroll", (y) => {
-  updateSizeJudul()
+  //. Pakai function yang ada di function.js
+  updateSizeJudul();
 });
+
+// ! Algoriitma matikan video saat tidak terlihat
+
+// ? Deklarasi variabel
+const video = document.querySelectorAll("video#pauseEffect");
+// ?  Logika kalau window di scroll
+document.addEventListener("scroll", () => {
+  video.forEach((element) => {
+    const videoRect = element.getBoundingClientRect();
+    const isVisible =
+      videoRect.top <= window.innerHeight && videoRect.bottom >= 0;
+
+    if (!isVisible && !video.paused) {
+      element.pause();
+    }
+    // console.log(videoRect+isVisible)
+  });
+});
+
+// ! Algoriitma animasi slider gambar
+let slider = document.getElementById("slider");
+let isDown = false;
+let startX, scrollLeft;
+
+slider.addEventListener("mousedown", (e) => {
+  e.preventDefault()
+  isDown = true;
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+  slider.classList.add("active");
+});
+
+slider.addEventListener("mouseleave", () => {
+  isDown = false;
+  slider.classList.remove("active");
+});
+
+slider.addEventListener("mouseup", () => {
+  isDown = false;
+  slider.classList.remove("active");
+});
+
+slider.addEventListener("mousemove", (e) => {
+  if (!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  const walk = (x - startX) * 6; //adjust this value to change the speed
+  slider.scrollLeft = scrollLeft - walk;
+});
+
+
