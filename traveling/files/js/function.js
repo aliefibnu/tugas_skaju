afec9d7a1c30aec2ede189f8a40e86e3c7e8fd28
aliefibnu <aliefarumi@gmail.com>
2024-10-02 23:19:52 +0700

    final commit

--- traveling/files/js/function.js
+++ traveling/files/js/function.js
@@ -1,3 +1,4 @@
+// ! Home Area
 function updateSizeJudul() {
   if (window.scrollY < 50) {
     elTitle.classList.remove("hidden");
@@ -20,4 +21,107 @@ function updateSizeJudul() {
     }, 500);
   }
   // document.title = window.scrollY;
+}
+
+// ! Tiket Pesawat Area
+
+/**
+ * @param {currentId}
+ */
+function showTable(currentId) {
+  tiketPesawatController(
+    data,
+    currentId,
+    usdToIdr,
+    elNavigation,
+    elHeader,
+    elTable,
+    elTeksHarga
+  );
+}
+
+/**
+ * @param {HTMLElement} elNavigation
+ * @param {String} id
+ */
+function changeNavTextColor(elNavigation, id) {
+  for (let iNav = 0; iNav < elNavigation.length; iNav++) {
+    if (elNavigation[iNav].id == id) {
+      elNavigation[iNav].classList.add("navActive");
+    } else {
+      elNavigation[iNav].classList.remove("navActive");
+    }
+  }
+}
+
+/**
+ * @param {HTMLElement} elHeader - Elemen header yang akan diubah teksnya.
+ * @param {string} id - id yang aktif saat ini.
+ */
+function changeHeaderText(elHeader, id) {
+  elHeader.innerText = data[id];
+}
+
+/**
+ * @param {HTMLElement[]} elTable - Daftar tabel yang akan ditampilkan atau disembunyikan.
+ * @param {string} id - id yang aktif saat ini.
+ */
+function changeTableDisplay(elTable, id) {
+  for (let iNav = 0; iNav < elTable.length; iNav++) {
+    if (elTable[iNav].id == id) {
+      elTable[iNav].style.display = "block";
+    } else {
+      elTable[iNav].style.display = "none";
+    }
+  }
+}
+
+/**
+ * @param {Object} data - Data konten.
+ * @param {Float32Array} usdToIdr - Kurs mata uang 1 dollar ke 1 rupiah.
+ * @param {HTMLElement[]} elTeksHarga - Daftar harga yang akan di konversi.
+ */
+function convertPrice(data, usdToIdr, elTeksHarga) {
+  for (let i = 0; i < elTeksHarga.length; i++) {
+    const lowPrice = data[elTeksHarga[i].dataset.maskapai].harga.low;
+    const topPrice = data[elTeksHarga[i].dataset.maskapai].harga.top;
+    const lowPriceIdr = lowPrice * usdToIdr;
+    const topPriceIdr = topPrice * usdToIdr;
+
+    elTeksHarga[
+      i
+    ].innerText = `$${lowPrice.toLocaleString()} - $${topPrice.toLocaleString()} (IDR: ${lowPriceIdr.toLocaleString()} - ${topPriceIdr.toLocaleString()})`;
+  }
+}
+
+/**
+ * Mengontrol tampilan tiket pesawat berdasarkan navigasi.
+ *
+ * @param {Object} data - Data konten.
+ * @param {string} startContentId - ID konten awal yang akan ditampilkan.
+ * @param {HTMLElement[]} elNavigation - Daftar elemen navigasi.
+ */
+function tiketPesawatController(
+  data,
+  startContentId,
+  usdToIdr,
+  elNavigation,
+  elHeader,
+  elTable,
+  elTeksHarga
+) {
+  for (let i = 0; i < elNavigation.length; i++) {
+    elNavigation[i].addEventListener("click", (x) => {
+      let id = x.target.id;
+      changeHeaderText(elHeader, id);
+      changeTableDisplay(elTable, id);
+      changeNavTextColor(elNavigation, id);
+    });
+
+    let id = startContentId;
+    changeHeaderText(elHeader, id);
+    changeTableDisplay(elTable, id);
+    changeNavTextColor(elNavigation, id);
+    convertPrice(data, usdToIdr, elTeksHarga);
+  }
 }
---
+++ traveling/files/js/tiket_pesawat.js
@@ -0,0 +1,59 @@
+let data,
+  startContentId,
+  usdToIdr,
+  elHeader,
+  elNavigation,
+  elTable,
+  elTeksHarga,
+  id,
+  iNav,
+  iTable;
+
+data = {
+  maskapai: "Lihat Maskapai Tersedia (Table 1)",
+  harga: "Cek Harga Tiket (Table 2)",
+  beli: "Beli Tiket Sekarang (Table 3)",
+  durasi: "Cek Durasi Penerbangan (Table 4)",
+  etihad: {
+    harga: {
+      low: 950,
+      top: 1400,
+    },
+  },
+  qatar_air: {
+    harga: {
+      low: 850,
+      top: 1400,
+    },
+  },
+  singapore_air: {
+    harga: {
+      low: 900,
+      top: 1500,
+    },
+  },
+  turkish_air: {
+    harga: {
+      low: 800,
+      top: 1300,
+    },
+  },
+};
+startContentId = "durasi";
+usdToIdr = 15279.8;
+elNavigation = document.getElementsByClassName("nav");
+elTable = document.getElementsByClassName("cTable");
+elHeader = document.getElementById("headerText");
+elTeksHarga = document.getElementsByClassName("teksHarga");
+
+tiketPesawatController(
+  data,
+  startContentId,
+  usdToIdr,
+  elNavigation,
+  elHeader,
+  elTable,
+  elTeksHarga
+);
+
+// ! ALIP YG BUAT JANGAN DICURI !
